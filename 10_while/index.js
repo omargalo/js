ce6777326d7948ad1fc6ba945e6768ced5cf268b
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 17:56:05 -0600

    Control de flujo while

---
+++ 10_while/index.js
@@ -0,0 +1,19 @@
+// Control de flujo while
+// Se queda iteranto mientras siga evaluando en true
+
+let x = 0
+
+while (x <= 5) {
+    console.log(x);
+    x++
+}
+
+console.log('Loop terminado');
+
+let y = true
+
+while (y) {
+    console.log(y);
+    y=false
+}
+console.log('"Y" fue evaluada una sola ves');
