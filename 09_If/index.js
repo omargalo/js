b231f015a745f84d59d17816ade28270999e450e
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 17:47:26 -0600

    Control de flujo: if

---
+++ 09_If/index.js
@@ -0,0 +1,14 @@
+// Control de flujo if
+// nos permite evaluar condiciones
+
+if (true) {
+    console.log('Estoy dentro de un if');
+}
+
+const edad = 22
+
+if (edad > 5 && edad < 18){
+    console.log('El niño puede jugar');
+} else {
+    console.log('Lo siento, no tienes la edad requerida');
+}
