5d7c5a514ffee6a51842fd027240364d0f8d3fe6
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 17:00:54 -0600

    Operadores de comparación

---
+++ 07_operadoresComparacion/index.js
@@ -0,0 +1,23 @@
+// Operadores de comparación
+
+// igualdad estricto (compara el valor y el tipo de dato)
+const resultado1= 5 === 6
+// igualdad no estricta (no compara el tipo de dato)
+const resultado2= 5 == '5'
+
+const resultado3= 5<6
+const resultado4= 5<5
+const resultado5= 5<6
+const resultado6= 5<4
+
+const resultado7= 5<=5
+const resultado8= 5<=6
+
+// desigualdad estricta
+const resultado9= 5!==6
+const resultado10= 5!='5'
+
+console.log(resultado1,resultado2);
+console.log(resultado3,resultado4,resultado5,resultado6);
+console.log(resultado7,resultado8);
+console.log(resultado9,resultado10);
