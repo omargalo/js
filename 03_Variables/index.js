0b8c8c5212da46880e763d8948e5d23c8253198b
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 14:14:54 -0600

    Variables, undefined y null

---
+++ 03_Variables/index.js
@@ -0,0 +1,34 @@
+// Variables
+// var actualmente ya no se usa
+// let
+// const
+
+//var miPrimeraVariable= 'bebe'
+
+let miPrimeraVariable='Este es el valor de mi variable'
+
+console.log(miPrimeraVariable);
+
+// Reasignando valor a una variable (Mutabilidad)
+
+miPrimeraVariable='Este valor ha cambiado'
+console.log(miPrimeraVariable);
+
+// boolean
+let miBoolean=true
+let miOtroBoolean=false
+
+// number
+let miNumero1=10
+let miNumero2=19
+let miNumero3=-271
+
+console.log(miNumero1,miNumero2,miNumero3,miBoolean,miOtroBoolean,miPrimeraVariable);
+
+// undefined
+let undef
+console.log(undef);
+
+// null
+let nulo=null
+console.log(nulo);
