94ee56a56ada72307b18ff209010e846afabc93d
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 17:27:03 -0600

    Operadores lógicos

---
+++ 08_operadoresLogicos/index.js
@@ -0,0 +1,14 @@
+// Operadores lógicos
+// or ||, and &&, not !
+
+// or evalua hasta encontrar el primer true
+const resultadoOr = false || false || 'Hola' || 'Evelyn'
+console.log(resultadoOr);
+
+// and evalua hasta encontrar el primer false
+const resultadoAnd = true && false
+console.log(resultadoAnd);
+
+// not
+const resultadoNot = !true
+console.log(resultadoNot);
