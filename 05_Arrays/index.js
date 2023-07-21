53b073acd05e371d691112d294ee848b8cac68bf
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 14:37:32 -0600

    Arrays

---
+++ 05_Arrays/index.js
@@ -0,0 +1,24 @@
+// Array vs List: En list los tipos de datos deben de ser del mismo tipo, no asi en los arrays.
+
+// Arreglo vacio
+const arregloVacio=[]
+
+// Arreglo
+const arreglo=[1,2,'Hola','Evelyn']
+
+console.log(arregloVacio, arreglo);
+
+// Agregar mas elementos al array
+arreglo.push(5)
+
+console.log(arreglo);
+
+// Agregando elementos al array vacio
+arregloVacio.push(5)
+arregloVacio.push(10)
+arregloVacio.push(true)
+arregloVacio.push(27)
+arregloVacio.push(33)
+arregloVacio.push('Evelyn')
+
+console.log(arregloVacio);
