57cc8a3fd8ff534acc778b1612090fbcd8305824
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 14:23:43 -0600

    Objetos

---
+++ 04_Objetos/index.js
@@ -0,0 +1,18 @@
+// Objeto: Es una agrupación de datos, estos datos deben de hacer sentido entre si.
+// Los objetos no tienen variables,
+// Los objetos tienen propiedades.
+
+// Objeto vacio
+const miPrimerObjeto={}
+
+// Objeto
+const miObjeto={
+    unNumero: 12,
+    unString:'Cadena de caracteres',
+    unaCondicion: true,
+}
+
+console.log(miObjeto);
+
+// Imprimir solamente una propiedad del objeto
+console.log(miObjeto.unString);
