13926069c6095d403e0f41f38431cf3d72d8b11f
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 16:47:38 -0600

    Operadores matemáticos

---
+++ 06_Operadores/index.js
@@ -0,0 +1,25 @@
+// Operadores
+
+const suma=1+2
+const restar=1-2
+const multiplicar=2*3
+const division=9/3
+
+console.log(suma,restar,multiplicar,division);
+
+// modulo
+const modulo=10%3
+console.log(modulo);
+
+// incremento y decremento
+let num=5
+num++
+console.log(num);
+num++
+console.log(num);
+num--
+console.log(num);
+num +=3
+console.log(num);
+num -=4
+console.log(num);
