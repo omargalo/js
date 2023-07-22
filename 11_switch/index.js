2c46a57697116dae0bda716f81f30428ab0e75e3
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 18:03:15 -0600

    Control de flujo switch

---
+++ 11_switch/index.js
@@ -0,0 +1,23 @@
+// Control de flujo switch
+
+let y=3
+
+switch (y) {
+    case 1: {
+        console.log('Caso 1');
+        break;
+    }
+    case 2: {
+        console.log('Caso 2');
+        break;
+    }
+    case 3: {
+        console.log('Caso 3');
+        break;
+    }
+    default: {
+        console.log('Caso Default');
+        break;
+    }
+}
+
