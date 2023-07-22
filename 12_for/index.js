c8ca4b04587ab057e9e483aeb25a3352e1fa6ba2
Omar Garcia <omar.garcia@omargl.net>
2023-07-21 18:17:15 -0600

    Control de flujo for

---
+++ 12_for/index.js
@@ -0,0 +1,20 @@
+// Control de flujo for
+// Nos permite iterar hasta que se cumpla una condición
+// Recibe 3 intrucciones:
+// valor inicial de nuestra variable
+// condicion que la variable tiene que cumplir
+// operación a realizar cuando finalize el loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+console.log('for en los arrays')
+// acceder a cada uno de los elementos de un array usando for
+
+const numeros = [1, 2, 3, 4, 5]
+
+for (let i = 0; i <= numeros.length; i++) {
+    console.log(i);
+}
+
