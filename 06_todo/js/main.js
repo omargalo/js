6c2d8eee7b9b1660a7f42e9296e49aefbfadb487
Omar Garcia <omar.garcia@omargl.net>
2023-07-24 13:28:36 -0600

    Agregando elementos al listado

--- 06_todo/js/main.js
+++ 06_todo/js/main.js
@@ -1,3 +1,11 @@
+// Para que podamos la referencia de nuestros todo's
+// tenemos que guardarlos en un espacio en memoria
+// para poder imprimirlos en una lista
+
+// Creamos una costante para almacenar los todo's
+// utilizando un arreglo vacio
+const todos = [];
+
 window.onload = () => {
     // Obtenemos la referencia del formulario
     const form =  document.getElementById('todo-form');
@@ -11,7 +19,21 @@ window.onload = () => {
         const todoText = todo.value;
         // Reemplazamos su valor con cadena vacia
         todo.value = '';
+        // Agregamos el valor al array "todos"
+        todos.push(todoText);
         // Imprimimos el valor en consola
         console.log(todoText);
+        // Construimos un html para mostrar los datos
+        // en una lista
+        // Es muy importante que la referencia este fuera
+        // del for para que no la vaya a buscar por la
+        // cantidad de "todos" que tengamos en nuestro listado
+        const todoList = document.getElementById('todo-list');
+        // Limpiamos el listado
+        todoList.innerHTML = '';
+        for(let i = 0; i < todos.length; i++) {
+            // Agregamos los elementos usando concatenacion
+            todoList.innerHTML += '<li>' + todos[i] + '</li>';
+        }
     }
 }
