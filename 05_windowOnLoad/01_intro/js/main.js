fbaed8eaa8ba8a9749bd116b2e2c8a0ae4e54a73
Omar Garcia <omar.garcia@omargl.net>
2023-07-24 12:49:37 -0600

    window on load

---
+++ 05_windowOnLoad/01_intro/js/main.js
@@ -0,0 +1,8 @@
+// window.onload
+// Espera a que se carguen todos los elementos html
+// antes de ejecutar el codigo JS
+
+window.onload = () => {
+    const parrafo = document.getElementById('text')
+    console.log(parrafo.innerHTML)
+}
